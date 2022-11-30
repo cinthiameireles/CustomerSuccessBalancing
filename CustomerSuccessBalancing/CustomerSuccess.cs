1b7d71ca8d4aafecb3bbf6a951a34c8015a47782
Cinthia Meireles <cinthia@consultalogo.com.br>
2022-11-30 12:16:55 -0300

    Add project files.

---
+++ CustomerSuccessBalancing/CustomerSuccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSuccessBalancing
+{
+    public class CustomerSuccess
+    {
+        public int CustomerSuccessBalancing(List<CustomerInfo> customerSuccess, List<CustomerInfo> costumers, List<int> costumerSuccessAway)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
